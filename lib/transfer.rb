6c534de4a8696da3b505b26bc5b47fa8896872f7
Aaron Samuel <aasamuelj@gmail.com>
2017-10-05 21:56:41 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,6 +15,8 @@ class Transfer
       self.sender.balance -= self.amount
       self.receiver.balance += self.amount
       self.status = "complete"
+    else 
+      "transaction rejected. Please check your account balance."
     end
   end
 end
